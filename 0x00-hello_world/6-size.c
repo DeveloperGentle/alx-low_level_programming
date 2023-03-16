43cf66f0f5103c4ed5a00925f8af91230cdc0984
Noyeosa Gentle <gnoyeosa@gmail.com>
2023-03-16 16:25:49 -0700

    Done with projects

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+printf("Size of a char: %ld byte(s)\nSize of an int: %ld byte(s)\nSize of a long int: %ld byte(s)\nSize of a long long int: %ld byte(s)\nSize of a float: %ld byte(s)\n",sizeof(char),sizeof(int),sizeof(long),sizeof(long long),sizeof(float));
+
+return 0;
+}
