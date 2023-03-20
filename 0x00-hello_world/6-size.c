ec9d4e993949a734e0a4f6b77a8ba319ac1292cd
Noyeosa Gentle <gnoyeosa@gmail.com>
2023-03-20 13:56:57 -0700

    Adde betty required argument, "void"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
 printf("Size of a char: %ld byte(s)\nSize of an int: %ld byte(s)\nSize of a long int: %ld byte(s)\nSize of a long long int: %ld byte(s)\nSize of a float: %ld byte(s)\n",sizeof(char),sizeof(int),sizeof(long),sizeof(long long),sizeof(float));
 
 return 0;
